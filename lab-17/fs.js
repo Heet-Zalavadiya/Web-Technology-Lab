ae983de165b477406bfbdbce22004b5a8f69acd0
Heet-Zalavadiya <heetzalavadia@gmail.com>
2025-07-14 17:57:01 +0530

    Lab-17

---
+++ lab-17/fs.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+// fs.exists('/path/to/file',(exists) =>{
+//     console.log(exists ? 'found' : 'not found!');
+// });
+// fs.stat('demo.js', (err,data) =>{
+//     console.log(data);
+// });
+// const data = fs.readFileSync('darshan.txt');
+// console.log(data.toString());
---
+++ lab-17/path.js
@@ -0,0 +1,8 @@
+const path = require('path');
+// console.log(path.normalize('/foo/abc/..'));
+// console.log(path.join('/foo','bar','abc'));
+// console.log(path.resolve('abc','xyz'));
+// console.log(path.relative('/foo/bar','/foo/abc'));
+// console.log(path.dirname('/foo/abc.txt'));
+// console.log(path.basename('/foo/abc.txt'));
+// console.log(path.extname('/foo/abc.txt'));
